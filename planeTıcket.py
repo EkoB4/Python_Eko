b824f3aeeb423ea65741847dbb890b88064184da
JREkin <41589534+JREkin@users.noreply.github.com>
2019-06-23 16:59:25 +0300

    Update planeTıcket.py

--- planeTıcket.py
+++ planeTıcket.py
@@ -1,5 +1,6 @@
 Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
+#JREkin
 >>> class uçak_bilet:
 	def kontrol_işlemleri(adsoyad, yas):
 		ad = str(input"""İsminiz ve soyadınız? : """)
@@ -9,4 +10,4 @@ Type "help", "copyright", "credits" or "license()" for more information.
 			print("Biletiniz hazır",adsoyad,"efendim bizi tercih etiğiniz için teşşekkürler")
 		else:
 			print("Yaşınız gereği bilet alamamakasınız",adsoyad,)
-			#First Original project JREkin#
+			# Original project JREkin#
